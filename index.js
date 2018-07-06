419246dcfdefe3e6ffe909a64004475b83830fb7
Abner <1154715918@qq.com>
2018-07-06 14:43:27 +0800

    Add some DOM operation function

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/* Utils Lib Function */
+
+
+/* -------------------About LocalStorage Operation------------------- */
+
 /**
  * 向LocalStorage写入数据
  * @param key 一个键值
@@ -32,4 +37,49 @@ function getItemFormLocalStorage(key, defaultValue) {
   }
 
   return item ? item : defaultValue;
+}
+
+/* -------------------About DOM Operation----------------------------- */
+
+/**
+ * 为某个元素添加样式
+ * @param el {DOM} Must
+ * @param className
+ */
+export function addClass(el, className) {
+  if (hasClass(el, className)) {
+    return;
+  }
+
+  let classNameList = el.className.split(' ');
+  classNameList.push(className);
+  el.className = classNameList.join(' ');
+}
+
+/*
+* 判断一个元素是否有某个样式的名称
+* @param el {DOM}
+* @param className {String} class name
+* @return {Boolean}
+ */
+export function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)');
+  return reg.test(el.className);
+}
+
+/**
+ * 获取属性的值
+ * @param el {dom}
+ * @param name
+ * @param val 不为undefined，说明是设置data-*的值
+ * @returns {string}
+ */
+export function getData(el, name, val) {
+  const prefix = 'data-';
+  let prop = prefix + name;
+  if (val) {
+    el.setAttribute(prop, val);
+  } else {
+    return el.getAttribute(prop);
+  }
 }
